f923d0e5469ab925f774a5e0dbec7ddedb4b2a36
kratos619 <gaurav.palaspagar@hotmail.com>
2019-02-16 10:07:35 +0530

    Initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    
+<H1>JSHDJA</H1>
+@endsection
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    
+<h1>service</h1>
+@endsection
