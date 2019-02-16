b478cc3e26d37a2a93936a70c60f8c392f46da3b
kratos619 <gaurav.palaspagar@hotmail.com>
2019-02-16 23:44:31 +0530

    hasOne Reletion
    address table has one reletion with user table

--- app/User.php
+++ app/User.php
@@ -27,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function address()
+    {
+        return $this->hasOne('App\Address', 'user_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,32 @@
 |
 */
 
+use App\User;
+use App\Address;
+
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/about', 'PagesController@index');
+// insert Data
+Route::get('/insert', function () {
+    $user = User::find(1);
+    $address = new Address(['name'=>'1403 D PuranikHomeTown Oval,Thane']);
+    $user->address()->save($address);
+});
+
+Route::get('/update', function () {
+    // find user
+    $address = Address::where('user_id', 1)->first();
+    $address->name = "Update New address 1403 D PuranikHomeTown Oval,Thane";
+    $address->save();
+});
+
+Route::get('/read', function () {
+    $user = User::find(1);
+    dd($user->address->name);
+});
+
 
 Auth::routes();
 
