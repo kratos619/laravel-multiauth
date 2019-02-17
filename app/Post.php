7edf7415312285fa152a5e8a81c0b3719048af10
kratos619 <gaurav.palaspagar@hotmail.com>
2019-02-17 18:22:09 +0530

    ....

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title','body'];
+
+    public function tags()
+    {
+        return $this->morphToMany('App\Tag', 'taggable');
+    }
 }
